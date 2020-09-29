02b0081e0a7614996b4888d4f9fafb3e94c6277d
ManhSyNguyen <manhns1511@gmail.com>
2020-09-29 18:06:16 +0700

    Update

--- DHM_Color/src/pages/views/Admin/Products/index.js
+++ DHM_Color/src/pages/views/Admin/Products/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types';
 import { Link, useHistory } from 'react-router-dom';
 
 const ProductsManager = ({ products, onRemove, categorys }) => {
