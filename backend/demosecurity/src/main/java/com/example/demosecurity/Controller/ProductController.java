6a368c72aa94d974ae0963788217b21eb0d2d2b8
nddduong2000 <ndduong2000@gmail.com>
2021-02-05 12:34:13 +0700

    Update

--- backend/demosecurity/src/main/java/com/example/demosecurity/Controller/ProductController.java
+++ backend/demosecurity/src/main/java/com/example/demosecurity/Controller/ProductController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("v1/api")
 public class ProductController {
     @Autowired
@@ -22,11 +22,11 @@ public class ProductController {
     @GetMapping("/products")
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
-    public List<ProductDTO> getAll() {
+    public List<ProductDTO> getAllProduct() {
         return productService.findAll();
     }
 
-    @GetMapping("/products/{id}/categorys")
+    @GetMapping("/products/categorys/{id}")
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
     public List<ProductDTO> getProductByCategory(@PathVariable(value ="id") long id) {
